7d22b5f11ded73f365ce56e038eae85df7613fe7
sakriymohamed <sakriymohamed@gmail.com>
2023-07-06 15:56:01 -0700

    updated oopConnectFour.js

---
+++ oopConnectFour.js
@@ -0,0 +1,48 @@
+"use strict";
+
+class Game {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.currPlayer = 1;
+    this.board = [];
+  }
+
+  makeBoard() {
+    for (let y = 0; y < this.height; y++) {
+      this.board.push(Array.from({ length: this.width }));
+    }
+  }
+
+  makeHtmlBoard() {
+    const board = document.getElementById('board');
+
+    // make column tops (clickable area for adding a piece to that column)
+    const top = document.createElement('tr');
+    top.setAttribute('id', 'column-top');
+    top.addEventListener('click', handleClick);
+
+    for (let x = 0; x < this.width; x++) {
+      const headCell = document.createElement('td');
+      headCell.setAttribute('id', x);
+      top.append(headCell);
+    }
+
+    board.append(top);
+
+    // make main part of board
+    for (let y = 0; y < this.height; y++) {
+      const row = document.createElement('tr');
+
+      for (let x = 0; x < this.width; x++) {
+        const cell = document.createElement('td');
+        cell.setAttribute('id', `c-${y}-${x}`);
+        row.append(cell);
+      }
+
+      board.append(row);
+    }
+  }
+}
+
+Game.makeBoard();
